ac58ce23e31e51d832b2210f9de524d131140e78
Alejocarto <alejocarto@hotmail.com>
2022-06-19 16:16:11 -0500

    Se agrega el proyecto

---
+++ src/App.js
@@ -0,0 +1,37 @@
+// El componente App es el padre de:
+// - Cabecera
+// - Listado
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+
+import {Component} from "react";
+import Cabecera from "./components/Cabecera";
+import Listado from "./components/Listado";
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      cantidad: 0
+    }
+  }
+
+  aumentarCantidad = () => {
+    this.setState({
+      cantidad: this.state.cantidad + 1
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Cabecera cantidad={this.state.cantidad} />
+        <Listado aumentarCantidad={this.aumentarCantidad} />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Item.js
@@ -0,0 +1,49 @@
+
+// El componente Item no tiene componentes hijos.
+// ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
+// MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
+// PROPS: Item recibe todos los campos que muestra en pantalla: nombre, descripcion, stock y el métodos heredados para su uso.
+// Maqueta de Item:
+//    h3
+//    p
+//    h5 > span    (este span debe mostrar la cantidad si es mayor a 0, "agotado" si llega a 0)
+//    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
+
+import {Component} from "react";
+
+class Item extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stock: props.data.stock
+    }
+  }
+
+  comprar = () => {
+    this.setState({
+      stock: this.state.stock - 1
+    })
+
+    this.props.aumentarCantidad();
+  }
+
+  render() {
+    return (
+      <div className='producto'>
+        {/* maquetar Item aquí */}
+        <h3> { this.props.data.producto.nombre } </h3>
+        <p> { this.props.data.producto.descripcion } </p>
+        <h5>
+          En stock:
+          {
+            this.state.stock === 0 ?  <span>agotado</span> :  this.state.stock
+          }
+
+        </h5>
+        <button onClick={this.comprar} disabled={this.state.stock === 0}> { this.state.stock === 0 ? 'SIN STOCK' : 'COMPRAR' } </button>
+      </div>
+    )
+  }
+}
+
+export default Item;
